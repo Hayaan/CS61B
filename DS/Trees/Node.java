a1398c102f7d54b7245ed03571960193be4e4538
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-05 13:44:13 +0200

    Binary Tree : Contains method (bool)

--- DS/Trees/Node.java
+++ DS/Trees/Node.java
@@ -7,18 +7,36 @@ class Node {
 
     public void insert(int value) {
         if (value <= data) {
-            if (left == null) {
+            if (left == null) {             // No left child = value not present.
                 left = new Node(value);
             } else {
                 left.insert(value);         // Recursively call the insert function on the current node's left child.
                                                                                                     }
         } else {
             if (value > data) {
-                if (right == null) {
+                if (right == null) {        // No right child = value not present.
                     right = new Node(value);
                 } else {
                     right.insert(value);    // Recursively call the insert function on the current node's right child.
                 }
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (data == value) {                // If the root == value, return true.
+            return true;
+        } else if (value < data) {
+            if (left == null) {             // If there is no left child, the value isn't present.
+                return false;
+            } else {                        // Else, recursively call the contains function on the left node.
+                return left.contains(value);
+            }
+        } else {                            // If there is no right child, the value isn't present.
+            if (right == null) {           
+                return false;
+            } else {                        // Else, recursively call the contains function on the right node.
+                return right.contains(value);
             }
         }
     }
