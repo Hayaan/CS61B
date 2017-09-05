5bd01e546f7cf10e97436619d403d7ae38279944
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-05 13:23:26 +0200

    Binary Tree: Insertion method

--- DS/Trees/Node.java
+++ DS/Trees/Node.java
@@ -4,4 +4,23 @@ class Node {
     public Node(int data) {
         this.data = data;
     }
+
+    public void insert(int value) {
+        if (value <= data) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.insert(value);         // Recursively call the insert function on the current node's left child.
+                                                                                                    }
+        } else {
+            if (value > data) {
+                if (right == null) {
+                    right = new Node(value);
+                } else {
+                    right.insert(value);    // Recursively call the insert function on the current node's right child.
+                }
+            }
+        }
+    }
 }
+
