8e78e75f06cc58b0d2939a59b738502d5857977a
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-06 19:56:35 +0200

    Lists 1, Vid 7: Linked IntList

---
+++ L2/IntList.java
@@ -0,0 +1,26 @@
+class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+    
+    public static void main(String[] args) {
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+/*
+        IntList L = new IntList();
+        L.first = 5;
+        L.rest = null;
+        
+        L.rest = new IntList();
+        L.rest.first = 10;
+
+        L.rest.rest = new IntList();    
+        L.rest.rest.first = 15;
+*/  
+    }
+}
