521e4c1f7578481cdea1578c4daa9fa39eb9dc9b
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-06 19:46:52 +0200

    Lists 1, Video 6: Arrays

---
+++ L2/Arrays.java
@@ -0,0 +1,12 @@
+class Arrays {
+    public static void main(String[] args) {
+        // Arrays are also Objects and objects are usually instantiated using 
+        // the new keyword.
+        Planet p = new Planet(0, 0, 0, 0, 0, "blah.png");
+        int[] x = new int[]{0, 1 ,2 , 95, 4};
+    
+        int[] a;                // Declaration
+        a = new int[]{0, 2, 3, 56};      // Instantiation & assignment
+
+    }
+}
