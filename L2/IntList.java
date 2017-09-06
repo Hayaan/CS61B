8bf7ac3db29715910dd35ff872a4d382fa050a36
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-06 20:32:35 +0200

    List 1, Vid 10: IntList exercises (get method)

--- L2/IntList.java
+++ L2/IntList.java
@@ -26,6 +26,25 @@ class IntList {
         return totalSize;
     }
 
+    // Will return ith item of this list.
+    public int get(int i) {             // My get method, opted to iterate
+        IntList p = this;
+        int currItem = 0;       
+        while (currItem != i) {
+            currItem++;
+            p = p.rest;
+        }
+        return p.first;
+    }
+
+    public int recursiveGet(int i) {    // Hug's get method, recursive.    
+        // Base case:
+        if ( i == 0) {
+            return first;
+        }
+        return rest.recursiveGet(i-1);
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
@@ -33,5 +52,7 @@ class IntList {
         
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
+        System.out.println(L.get(2));
+        System.out.println(L.recursiveGet(2));
     }
 }
