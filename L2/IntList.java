3c0cc415653efa4b73fd43b0f10010baf9c548ea
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-06 20:10:02 +0200

    List 1, Vid 8: Adding a recursive size method to the Linked IntList

--- L2/IntList.java
+++ L2/IntList.java
@@ -6,21 +6,32 @@ class IntList {
         first = f;
         rest = r;
     }
+    /** Return the size of the list using recursion! */
+    public int size() {
+        if (rest == null) {     // If the rest is null, you're at the last node.
+            return 1;
+       }    
+       return 1 + this.rest.size(); // Else, recursively call size on rest.
+    }
     
+    /** Returns the size of the list w/o recursion*/
+    public int iterativeSize() {
+        IntList p = this;       // Create a pointer variable.
+        int totalSize = 0;
+
+        while (p != null) {
+            totalSize++;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
-/*
-        IntList L = new IntList();
-        L.first = 5;
-        L.rest = null;
         
-        L.rest = new IntList();
-        L.rest.first = 10;
-
-        L.rest.rest = new IntList();    
-        L.rest.rest.first = 15;
-*/  
+        System.out.println(L.size());
+        System.out.println(L.iterativeSize());
     }
 }
