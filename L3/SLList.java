27f0153a98683bdfb04eff637f9049c8b7310db2
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-07 22:05:47 +0200

    Modifications to the nested IntNode class

--- L3/SLList.java
+++ L3/SLList.java
@@ -3,11 +3,15 @@ public class SLList {
     // *Encapsulation*
     private IntNode head;
 
-    class IntNode {
-        public int item;
-        public IntNode next;
+    // Nested class
+    // Useful for when the class is 'dependent' on the main class.
+    private static class IntNode {
+        // Doesn't use outside class' instance variables or methods
+        // so it can be declared static.
+        int item;
+        IntNode next;
 
-        public IntNode(int i, IntNode n) {
+        IntNode(int i, IntNode n) {
             this.item = item;
             this.next = next;
         }
@@ -32,16 +36,16 @@ public class SLList {
         SLList L = new SLList(10);
         L.addFirst(10);
         L.addFirst(5);
-        System.out.println(L.getFirst());        
+        System.out.println(L.getFirst());
     }
 
     /* Something to watch out for is not using encapsulation to avoid ...
-     * encountering any problems, such as 
+     * encountering any problems, such as
      *
      * SLList L = new SLList(15);
      * L.addFirst(10);
-     * L.first.next.next = L.first.next;        
-     * 
+     * L.first.next.next = L.first.next;
+     *
      * The node containing the item 15 ends up pointing to itself &
      * thus messing up the list.
      */
