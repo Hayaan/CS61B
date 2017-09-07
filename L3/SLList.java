da9a402c5854dd6db6322c2169b328b75276c1a7
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-07 21:28:02 +0200

    User-friendly SLL w/ prepend & getHead methods

--- L3/SLList.java
+++ L3/SLList.java
@@ -4,7 +4,7 @@ public class SLList {
         public IntNode next;
 
         public IntNode(int item, IntNode next) {
-            this.item = value;
+            this.item = item;
             this.next = next;
         }
     }
@@ -15,8 +15,21 @@ public class SLList {
         head = new IntNode(x, null);
     }
 
+    /** Prepends x*/
+    public void addFirst(int x) {
+        head = new IntNode(x, head);
+    }
+
+    /** Returns the first item in a list */
+    public int getFirst() {
+        return head.item;
+    }
+
     public static void main(String[] args) {
         /* Creates a list of one integer, namely 10 */
         SLList L = new SLList(10);
+        L.addFirst(10);
+        L.addFirst(5);
+        System.out.println(L.getFirst());        
     }
 }
