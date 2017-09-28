66fbe86dd65f84e1d96665a0d01bb3106b635ad5
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-28 21:49:46 +0200

    Proj0: The Planet class and its constructors

--- NBody/Planet.java
+++ NBody/Planet.java
@@ -1,2 +1,23 @@
 public class Planet {
+    public double xxPos, yyPos, xxVel, yyVel, mass;
+    public String imgFileName;
+
+    public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet planet) {
+        planet.xxPos = xxPos;
+        planet.yyPos = yyPos;
+        planet.xxVel = xxVel;
+        planet.yyVel = yyVel;
+        planet.mass = mass;
+        planet.imgFileName = imgFileName;
+    }
+
 }
