efc9ca09010201659b48bf5afb06e50870f980a6
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-09-28 22:41:38 +0200

    Proj0: first itereration of calcDistance

--- NBody/Planet.java
+++ NBody/Planet.java
@@ -11,6 +11,7 @@ public class Planet {
         this.imgFileName = imgFileName;
     }
 
+// Makes a copy of the passed in object (copies the values of all the fields into a new object)
     public Planet(Planet planet) {
         this.xxPos = planet.xxPos;
         this.yyPos = planet.yyPos;
@@ -20,4 +21,17 @@ public class Planet {
         this.imgFileName = planet.imgFileName;
     }
 
+    public calcDistance(Planet otherPlanet) {
+        double deltaX = this.xxPos - otherPlanet.xxPos; 
+        deltaX_squared = deltaX*deltaX;
+        double deltaY = this.yyPos - otherPlanet.yyPos;
+        deltaY_squared = deltaY*deltaY;
+
+        double r_squared = deltaX_squared + deltaY_squared;
+        return Math.sqrt(r_squared);
+    }
+
+    public static void main(String[] args) {
+        Planet Saturn = new Planet()
+    }
 }
