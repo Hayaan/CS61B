7be78622faf06841e8c38fdc2240c17e1e309af4
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-10-01 20:51:41 +0200

    ArrayList: Implementation of the basic methods

--- L4/AList.java
+++ L4/AList.java
@@ -3,40 +3,52 @@
 // ArrayLists rather than your generic LinkedList that makes use of pointers
 // to have O(1) access time complexity.
 
-/** Methods:
-    addLast()
-    getLast()
+/* Methods & invariants:
+    addLast():
+        the next item we want to add, will go into position (size)
+    getLast():
+        the item we want to return is in position (size-1)
     removeLast()
     get(int i)
+    size:
+        # of items in the arraylist should == size
 */
 public class AList {
+    int[] values;    
+    int size;
+
     /** Creates an empty arraylist */
     public AList() {
-
+        items = new int[100];
+        size = 0;
     }
 
     /** Adds an integer to the back of the arraylist */
     public void addLast(int num) {
-
+        values[size] = num;
+        size++;
     }
 
     /** Returns the last value of the arraylist */
     public int getLast() {
-        return 0;
+        return values[size-1];
     }
 
     /** Return value @ ith index in the array list. Zero-based */
     public int get(int i) {
-        return 0;
+        return values[i];
     }
 
     /** Return the size of the arraylist */
     public int size() {
-        return 0;
+        return this.size;
     }
 
     /** Remove and return the last value in the arraylist */
     public int removeLast() {
-        return 0;
+        int value = values[size-1];
+        values[size-1] = 0;
+        size--;
+        return value;
     }
 }
