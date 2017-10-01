b370e2aad49fb40c7fad36a1b05ddf582a3daab9
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-10-01 20:26:02 +0200

    ALists: Why ArrayLists over your generic LinkedLists?

---
+++ L4/AList.java
@@ -0,0 +1,5 @@
+// Arrays > LinkedLists w/r/t accessing data at a certain index
+// which is why for a large list, it might be beneficial to use 
+// ArrayLists rather than your generic LinkedList that makes use of pointers
+// to have O(1) access time complexity.
+ 
