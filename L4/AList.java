c1a982f1468e344688b3c84ed0d254d11fb48de7
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-10-01 20:33:57 +0200

    ArrayList: Skeleton class & methods, along with the reqs

--- L4/AList.java
+++ L4/AList.java
@@ -10,5 +10,33 @@
     get(int i)
 */
 public class AList {
-    
+    /** Creates an empty arraylist */
+    public AList() {
+
+    }
+
+    /** Adds an integer to the back of the arraylist */
+    public void addLast(int num) {
+
+    }
+
+    /** Returns the last value of the arraylist */
+    public int getLast() {
+        return 0;
+    }
+
+    /** Return value @ ith index in the array list. Zero-based */
+    public int get(int i) {
+        return 0;
+    }
+
+    /** Return the size of the arraylist */
+    public int size() {
+        return 0;
+    }
+
+    /** Remove and return the last value in the arraylist */
+    public int removeLast() {
+        return 0;
+    }
 }
