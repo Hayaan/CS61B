9e7fa6d0727bf319963af9e0ad05bafb2702f4c9
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2017-12-26 11:49:26 +0100

    Proj0: NBody methods work along with storing input for the initial universe state

--- NBody/NBody.java
+++ NBody/NBody.java
@@ -17,11 +17,14 @@ public class NBody {
 		return 0;
 	}
 
-	public static void readPlanets(String fileLoc) {
+	public static Planet[] readPlanets(String fileLoc) {
 		try {
 			Scanner reader = new Scanner(new FileReader(fileLoc));
 
-			int n = reader.nextInt(); reader.nextLine();
+			int n = reader.nextInt(); 
+			reader.nextLine();
+			reader.nextLine();
+
 			Planet[] planets = new Planet[n];
 
 			for (int i = 0; i < n; i++) {
@@ -31,16 +34,29 @@ public class NBody {
 				xP = reader.nextDouble();
 				yP = reader.nextDouble();
 				xV = reader.nextDouble();
-				yV = reader.
-			} catch (FileNotFoundException e) {
+				yV = reader.nextDouble();
+				mass = reader.nextDouble();
+				image = reader.nextLine().trim();
+				planets[i] = new Planet(xP, yP, xV, yV, mass, image);
+			}
+			return planets;
+		} catch (FileNotFoundException e) {
 			System.out.println("The file could not be found.");
 		}
+		Planet[] fail = new Planet[1];
+		return fail;
 	}
 
 	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		double T, dt, radius;
+		String filename;
+		Planet[] planets;
 
-		System.out.println(readRadius("data/planets.txt"));
-		readPlanets("data/planets.txt");
+		T = Double.parseDouble(args[0]);
+		dt = Double.parseDouble(args[1]);
+		filename = args[2].trim();
+		radius = readRadius(filename);
+		planets = readPlanets(filename);
 	}
-
 }
--- NBody/Planet.java
+++ NBody/Planet.java
@@ -108,6 +108,10 @@ public class Planet {
         return netForce.doubleValue();
     }
 
+    public String toString() {
+        return "Name: " + this.imgFileName + " mass: " + this.mass;
+    }
+
     public static void main(String[] args) {
         Planet Saturn = new Planet(2.3e12, 9.5e11, 0, 0, 6.0e26, "");
         Planet Sun = new Planet(1.0e12, 2.0e11, 0, 0, 2.0e30, "");
