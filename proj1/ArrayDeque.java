4a08eed1ac1446ba4e5c5133bf6cf12dfbcf16bf
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2018-02-25 21:59:18 +0100

    Proj1: Array Deque skeleton

---
+++ proj1/ArrayDeque.java
@@ -0,0 +1,50 @@
+public class ArrayDeque<T> {
+    
+    public ArrayDeque() {
+
+    }
+    
+    /* Prepend item to the Linked List, if it's the first. It'll be the sentinel node's next and previous node. */
+    public void addFirst(T item) {
+
+    }
+
+    /* Append item to the Linked List, if it's the first. It'll be the sentinel node's next and previous node. */
+    public void addLast(T item) {
+
+    }
+
+    /* Return true if empty, false if the List contains any items. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /* Returns the # of items in the Deque */
+    public int size() {
+        return this.size;
+    }
+
+    /* Print deque items. Each item will be seperated by a single space. */
+    public void printDeque() {
+
+    }
+
+    /* Removes and returns the first item in the Linked List */
+    public T removeFirst() {
+
+    }
+ 
+    /* Removes and returns the last item in the Linked List */    
+    public T removeLast() {
+
+    }
+
+    /* Get i'th item in the list */
+    public T get(int index) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
