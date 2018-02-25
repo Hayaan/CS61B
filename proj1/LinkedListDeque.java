d7a08a7df778c195f38c879bcdf1fed607a06aa2
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2018-02-25 17:34:37 +0100

    Proj1: Doc for Deque's methods

--- proj1/LinkedListDeque.java
+++ proj1/LinkedListDeque.java
@@ -18,31 +18,38 @@ public class LinkedListDeque<T> {
 
     }
 
+    /* Append item to the Linked List, if it's the first. It'll be the sentinel node's next and previous node. */
     public void addLast(T item) {
 
     }
 
+    /* Return true if empty, false if the List contains any items. */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /* Returns the # of items in the Deque */
     public int size() {
         return this.size;
     }
 
+    /* Print deque items. Each item will be seperated by a single space. */
     public void printDeque() {
 
     }
 
+    /* Removes and returns the first item in the Linked List */
     public T removeFirst() {
 
     }
-
+ 
+    /* Removes and returns the last item in the Linked List */    
     public T removeLast() {
 
     }
 
+    /* Get i'th item in the list */
     public T get(int index) {
-        
+
     }
 }
