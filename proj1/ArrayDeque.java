01702f7871d72cda4f14b896420b16651ca5709e
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2018-02-27 11:18:56 +0100

    Proj1: Arr Deque add methods

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -1,17 +1,38 @@
 public class ArrayDeque<T> {
-    
-    public ArrayDeque() {
+    private T[] data;
+    private int size, nextFirst, nextLast;
 
+    public ArrayDeque() {
+        data = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 3;
+        nextLast = 4;
+    }    
+    
+    public ArrayDeque(T item) {
+        data = (T[]) new Object[8];
+        data[4]= item;
+        size = 1;
+        nextFirst = 3;
+        nextLast = 5;
     }
     
     /* Prepend item to the Linked List, if it's the first. It'll be the sentinel node's next and previous node. */
     public void addFirst(T item) {
-
+        data[nextFirst] = item;
+        size++; nextFirst++;
+        if (size == data.length) {
+            resize();
+        }
     }
 
     /* Append item to the Linked List, if it's the first. It'll be the sentinel node's next and previous node. */
     public void addLast(T item) {
-
+        data[nextLast] = item;
+        size++; nextLast++;
+        if (size == data.length) {
+            resize();
+        }
     }
 
     /* Return true if empty, false if the List contains any items. */
@@ -45,6 +66,17 @@ public class ArrayDeque<T> {
     }
 
     public static void main(String[] args) {
-
+        ArrayDeque<Integer> test = new ArrayDeque<>();
+        test.printDeque();
+        test.addFirst(2);
+        System.out.println(test.size());
+        
+        Random rand = new java.util.Random();
+        for (int i = 0; i < 10; i++) {
+            test.addFirst(rand.nextInt());
+            test.addLast(rand.nextInt());
+        }
+    
+        System.out.println(test.size());
     }
 }
