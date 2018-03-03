52785d472f0eb254e62396efb212962f49b67f18
Hayaan Abdi <hayaanabdi96+github@gmail.com>
2018-03-03 21:05:04 +0100

    Proj1: ADQ restart

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -9,47 +9,18 @@ public class ArrayDeque<T> {
         nextLast = 5;
     }
 
-    public void addFirst(T item) {
-        data[nextFirst] = item;
-        // TODO:
-        nextFirst--;
-        // Make sure nextFirst rolls over to data.length once it becomes
-        // smaller than 0.
-        size++;
-        if (size == data.length) {
-            resize(size * 2);
-        } else if (.25*size <= data.length && data.length > 8) {
-            resize((int) (size * 0.5));
-        }
-    }
 
-    public void addLast(T item) {
-        data[nextLast] = item;
-        /*  Rolls nextLast over to 0 once the index exceeds
-            the bounds of the array */
-        nextLast = (nextLast + 1) % (data.length - 1);
-        size++;
-        if (size == data.length) {
-            resize(size * 2);
-        } else if (.25*size <= data.length && data.length > 8) {
-            resize((int) (size * 0.5));
+    public static void main(String[] args) {
+        ArrayDeque<Integer> test = new ArrayDeque<>();
+        java.util.Random rand = new java.util.Random();
+        for (int i = 0; i < 6; i++) {
+            test.addFirst(rand.nextInt(101));
+            test.addLast(rand.nextInt(101));
         }
-    }
 
-    public void resize(int capacity) {
-        T[] temp = (T[]) new Object[capacity];
+        System.out.println(test.size());
 
-        if (capacity > data.length) {
-            for (int i = 0; i < capacity; i++) {
-                temp[i] = data[(nextLast + i) % data.length];
-            }
-        } else {
-            for (int i = 0; i < size; i++) {
-                temp[i] = data[nextFirst + i + 1];
-            }
-        }
-        nextFirst = temp.length; 
-        nextLast = size;
-        data = temp;
+        test.printDeque();
+
     }
 }
