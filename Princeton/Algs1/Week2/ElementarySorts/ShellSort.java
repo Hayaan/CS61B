6d485c643dc5e6e6307b3c51b8ba362a0c853a0d
Hayaan <hayaanabdi96+github@gmail.com>
2018-06-30 00:41:28 +0200

    [Alg 4.1] {Elementary sorts}

--- Princeton/Algs1/Week2/ElementarySorts/ShellSort.java
+++ Princeton/Algs1/Week2/ElementarySorts/ShellSort.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class InsertionSort {
-    private static void exchange (Comparable[] arr, int i, int j) {
-        Comparable temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+public class ShellSort {
+    private static void exchange(Comparable[] arr, int i, int j) {
+        Comparable temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     }
 
     private static boolean isLess(Comparable a, Comparable b) {
@@ -19,27 +19,29 @@ public class InsertionSort {
         return true;
     }
 
+    /* 
+    Sometimes doesn't properly sort. Switch if to while?
+    */
     public static void sort(Comparable[] arr) {
-        int swapIndex, sortedIndex;
-        for (int i = 1; i < arr.length; i++) {
-            swapIndex = i;
-            sortedIndex = i - 1;
-            while (sortedIndex >= 0 && isLess(arr[swapIndex], arr[sortedIndex])) {
-                exchange(arr, swapIndex--, sortedIndex--);
+        for (int h = arr.length / 2; h > 0; h--) {
+            for (int i = 0; (i + h) < arr.length; i++) {
+                if (isLess(arr[i + h], arr[i])) {
+                    exchange(arr, i + h, i);
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int n = rand.nextInt(8) + 8;
+        int n = rand.nextInt(24) + 1;
         Integer[] arr = new Integer[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = rand.nextInt(101);
+            arr[i] = rand.nextInt(100) + 1;
         }
         System.out.println("Pre sort:\t" + Arrays.toString(arr));
-        InsertionSort.sort(arr);
+        ShellSort.sort(arr);
         System.out.println("Post sort:\t" + Arrays.toString(arr));
-        System.out.println("Sorted check:\t" + InsertionSort.isSorted(arr));
+        System.out.println("Sorted check:\t" + ShellSort.isSorted(arr));
     }
 }
